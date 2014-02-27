f0c4fd397c3c5f18be93cc9cf11abde7dd60f6c3
Neo Chen <neoxen@qq.com>
2014-02-27 17:03:41 +0800

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockwatch::Application.config.secret_key_base = '6a1fffa4f641eb84c7ed2af93ddfedd99765d7593849599d40b793eaf3f457ad414af08b88498568210c9b79f6d037970ded5810654c882cc1e11b680cb3a38e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stockwatch::Application.config.session_store :cookie_store, key: '_stockwatch_session'
